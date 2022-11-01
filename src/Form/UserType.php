13920a06aaa9f29584ff3f0477fd8aa859356a39
Diego García <dgarciaortiz94@gmail.com>
2022-11-01 23:56:35 +0100

    entity errors mapped

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,13 +16,45 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('name')
-            ->add('firstname')
-            ->add('lastname')
+            ->add('email', EmailType::class, [
+                'error_bubbling' => true,
+                'attr' => [
+                    'maxlength' => false,
+                    'placeholder' => 'Email',
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'error_bubbling' => true,
+                'attr' => [
+                    'maxlength' => false,
+                    'placeholder' => 'password',
+                ]
+            ])
+            ->add('name', TextType::class, [
+                'error_bubbling' => true,
+                'attr' => [
+                    'maxlength' => false,
+                    'placeholder' => 'name',
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'error_bubbling' => true,
+                'attr' => [
+                    'maxlength' => false,
+                    'placeholder' => 'firstname',
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'error_bubbling' => true,
+                'attr' => [
+                    'maxlength' => false,
+                    'placeholder' => 'lastname',
+                ]
+            ])
             ->add('image', FileType::class, [
+                'error_bubbling' => true,
                 'required' => false,
+                'label' => 'Image',
             ])
         ;
     }
